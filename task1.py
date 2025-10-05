43d19c6ad7df54bb7509e85f61a8e3fd1df66f33
System Administrator <root@MacBook-Air-Anastasia.local>
2025-10-05 18:53:10 +0300

    Homework first pack

---
+++ task1.py
@@ -0,0 +1,17 @@
+# todo: Определить в коде переменные:
+# 1. Целочисленного типа
+# 2. Вещественного типа
+# 3. Логического типа
+# 4. Строкового типа
+# 5. Пустого типа
+# Вывести их типы (надо погуглить).
+integer_type :int = 1
+float_type :float = 0.2
+bool_type :bool = True
+string_type :str = 'hello'
+null_type :None = None
+print('Данный тип переменной, ', type(integer_type))
+print('Данный тип переменной, ', type(float_type))
+print('Данный тип переменной, ', type(bool_type))
+print('Данный тип переменной, ', type(string_type))
+print('Данный тип переменной, ', type(null_type))
---
+++ task5.py
@@ -0,0 +1,14 @@
+#todo: Написать программу, которая считывает два числа и выводит их сумму, разность, частное, произведение,
+# результат целочисленного деления, результат деления с остатком, результат возведения в степень.
+a=int(input('a = '))
+b=int(input('b = '))
+print('Summ = ', a+b)
+print('Raznost = ', a-b)
+print('Proizvedenie = ', a*b)
+print('Chastnoe = ', a/b)
+print('Celochislennoe = ', a//b)
+print('Ostatok = ', a%b)
+print('Stepen a = ', a*a)
+print('Stepen b = ', b*b)
+
+
