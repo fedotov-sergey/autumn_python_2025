4a18889d8c2db271f11ab742f1404bffe4013f86
fedotov-sergey <FedotovOfc@gmail.com>
2025-10-30 20:41:19 +0300

    POWER OF BIBLE

--- task41.py
+++ task41.py
@@ -20,38 +20,76 @@
 # Создает XML структуру с корневым элементом <report>
 # HTMLExporter (дополнительно):
 # Создает красивую HTML-таблицу с CSS-стилями
-class DataExporter:
-    def __init__(self, data):
-            self.data = data
-    def get_format_name(self):
+import csv
+import json
+import xmltodict
+from datetime import datetime
 
-    def validate_date(self,date):
+
+class DataExporter:
+    def export(self, data):
+        pass
+
+    def get_format_name(self):
+        pass
+
+    def validate_data(self, data):
+        if len(data) == 0:
+            print(f"{data} is empty")
+            return False
+        else:
+            print(f"{data} is valid")
+            return True
+
 
 class JSONExporter(DataExporter):
-    def __init__(self, data):
-            super().__init__(data)
-    def get_format_name(self):
+    def export(self, data):
+        if not self.validate_data(data):
+            return
+        dt = {"time": datetime.now().isoformat(), "data": data}
+        file = json.dumps(dt, ensure_ascii=False, indent=4)
+        f = open("jsonfile.json", "w", encoding="utf-8")
+        f.write(file)
+        f.close()
 
-    def validate_date(self,date):
+    def get_format_name(self):
+        return "json"
+
 
 class CSVExporter(DataExporter):
-    def __init__(self, data):
-            super().__init__(data)
+    def export(self, data):
+        if not self.validate_data(data):
+            return
+        fieldnames = data[0].keys()
+        f = open("csvfile.csv", "w", newline="", encoding="utf-8")
+        cvsfile_ = csv.DictWriter(f, fieldnames=fieldnames)
+        cvsfile_.writeheader()
+        for row in data:
+            cvsfile_.writerow(row)
+        f.close()
+
     def get_format_name(self):
+        return "csv"
 
-    def validate_date(self,date):
 
-class HTMLExporter(DataExporter):
-    def __init__(self, data):
-            super().__init__(data)
+class XMLExporter(DataExporter):
     def get_format_name(self):
+        return "xml"
 
-    def validate_date(self,date):
+    def export(self, data):
+        if not self.validate_data(data):
+            return
+        xml_data = {"report": {"item": data}}
+        xml_string = xmltodict.unparse(xml_data, pretty=True)
+        f = open("xmlfile.xml", "w", encoding="utf-8")
+        f.write(xml_string)
+        f.close()
+
 
 # Этот код должен работать после реализации:
 sales_data = [{"product": "Laptop", "price": 1000, "quantity": 2}, {"product": "Mouse", "price": 50, "quantity": 10}]
 
-exporters = [JSONExporter(), CSVExporter(), XMLExporter()]
+exporters = [CSVExporter(), XMLExporter(), JSONExporter()]
 
 for exporter in exporters:
     print(f"Формат: {exporter.get_format_name()}")
